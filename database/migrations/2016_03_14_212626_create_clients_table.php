ef676a453728985f434d0a9239ee8c6ffd2af3da
Mercality <ramonlv93@gmail.com>
2016-03-14 17:19:12 -0430

    Create clients, orders and details migrations

---
+++ database/migrations/2016_03_14_212626_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('client_id')->;
+            $table->tinyInteger('lines');
+            $table->integer('subtotal');
+            $table->integer('tax');
+            $table->integer('total');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
