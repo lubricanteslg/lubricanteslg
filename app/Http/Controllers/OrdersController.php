2d7076731ac9fd003c76fcb93aa73b02454ea5f6
Mercality <ramonlv93@gmail.com>
2016-03-16 16:19:00 -0430

    Create method for showing a list of Orders with pagination

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -14,9 +14,9 @@ class OrdersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return \App\Order::paginate(10);
     }
 
     /**
