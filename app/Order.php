e2ffca974f6d69f3e01d149930ee47ef8bc1434e
Mercality <ramonlv93@gmail.com>
2016-03-16 10:28:42 -0430

    Create Validations for Order and OrderDetails

--- app/Order.php
+++ app/Order.php
@@ -6,6 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    public static $rules = array(
+                                'date' => array('required', 'date'),
+                                'subtotal' => array('required', 'numeric'),
+                                'tax' => array('required', 'numeric'),
+                                'total' => array('required', 'numeric'),
+                                'salesman_id' => array('required', 'exists:salesmen,id'),
+                                'client_id' => array('required', 'exists:clients,id'),
+                                'detail' => array('array')
+        );
+
+    public static function validate($input)
+    {
+        $v = \Validator::make($input, static::$rules);
+
+        return $v->fails()
+                ? $v
+                : true;
+    }
+
     public function client() {
         return $this->belongsTo('App\Client');
     }
--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -6,6 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
+        public static $rules = array(
+                                    'product_code' => array('required', 'exists:products,code'),
+                                    'product_desc' => array('required', 'alpha_num'),
+                                    'qty' => array('required', 'integer'),
+                                    'price' => array('required', 'numeric'),
+            );
+
+        public static function validate($input)
+        {
+            $v = \Validator::make($input, static::$rules);
+
+            return $v->fails()
+                    ? $v
+                    : true;
+        }
+
+        public static function validateMany($input) {
+            foreach($input as $detail)  {
+                $v = static::validate($detail);
+                if ($v !== true) return $v;
+            }
+
+            return true;
+        }
+
     public function order() {
         return $this->belongsTo('App\Order');
     }
